5037d46f422596927a626b06dc9de7787f075471
Eric Lovell <eric1992@vt.edu>
2014-08-27 11:50:39 -0400

    Intial Commit, added base interface

---
+++ Eric_Crypto_Library/Eric_Crypto_Library/CryptoSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eric_Crypto_Library
+{
+    public interface CryptoSystem
+    {
+        public IEnumerable<char> getPlainText();
+        public IEnumerable<char> getCipherText();
+        public IEnumerable<string> getPossibleKeys();
+        public IEnumerable<char> encrypt(IEnumerable<char> input, string key);
+        public IEnumerable<char> decrypt(IEnumerable<char> input, string key);
+    }
+}
