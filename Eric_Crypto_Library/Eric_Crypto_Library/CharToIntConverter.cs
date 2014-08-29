d1f492f6146f12b0c0c1639cda8aff75c6e85fdc
Eric Lovell <eric1992@vt.edu>
2014-08-29 11:21:18 -0400

    Added invalid key exception and chartointconverter

---
+++ Eric_Crypto_Library/Eric_Crypto_Library/CharToIntConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+
+namespace Eric_Crypto_Library
+{
+    public class CharToIntConverter
+    {
+        public static Dictionary<char, int> Mappings = new Dictionary<char, int>()
+        {
+            {'a', 0},
+            {'b', 1},
+            {'c', 2},
+            {'d', 3},
+            {'e', 4},
+            {'f', 5},
+            {'g', 6},
+            {'h', 7},
+            {'i', 8},
+            {'j', 9},
+            {'k', 10},
+            {'l', 11},
+            {'m', 12},
+            {'n', 13},
+            {'o', 14},
+            {'p', 15},
+            {'q', 16},
+            {'r', 17},
+            {'s', 18},
+            {'t', 19},
+            {'u', 20},
+            {'v', 21},
+            {'w', 22},
+            {'x', 23},
+            {'y', 24},
+            {'z', 25}
+        };
+        public static int Convert(char input)
+        {
+            return Mappings[input];
+        }
+
+        public static char Convert(int input)
+        {
+            return Mappings.First(kp => kp.Value == input).Key;
+        }
+
+    }
+}
