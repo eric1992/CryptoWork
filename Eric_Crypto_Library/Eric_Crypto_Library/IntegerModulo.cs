beb3a21d2ce1af9d1ea295bb529a52d54aa1f4a6
eric1992 <eric1992@vt.edu>
2014-09-16 17:48:44 -0400

    Added integer modulo n class

---
+++ Eric_Crypto_Library/Eric_Crypto_Library/IntegerModulo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using MathNet.Numerics;
+
+namespace Eric_Crypto_Library
+{
+    public class IntegerModulo
+    {
+        private int _modulo;
+
+        public int Modulo
+        {
+            get { return _modulo; }
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Cannot have a negative or zero modulo");
+                }
+                _modulo = value;
+            }
+        }
+
+        private int _value;
+
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                var current = value;
+                if (current < 0)
+                {
+                    do
+                    {
+                        current = _modulo + current;
+                    } while (current < 0);
+                    _value = current;
+                }
+                else
+                {
+                    _value = current % _modulo;
+                }
+            }
+        }
+
+        public IntegerModulo(int modulo, int value)
+        {
+            Modulo = modulo;
+            Value = value;
+        }
+
+
+        public static IntegerModulo operator +(IntegerModulo a, IntegerModulo b)
+        {
+            if(a.Modulo != b.Modulo)
+                throw new Exception("Cannot add IntegerModulos of different modulos.");
+            return new IntegerModulo(a.Modulo, a.Value+b.Value);
+        }
+
+        public static IntegerModulo operator *(IntegerModulo a, IntegerModulo b)
+        {
+            if (a.Modulo != b.Modulo)
+                throw new Exception("Cannot multiply IntegerModulos of different modulos.");
+            return new IntegerModulo(a.Modulo, a.Value * b.Value);
+        }
+
+        public static IntegerModulo operator -(IntegerModulo a, IntegerModulo b)
+        {
+            if (a.Modulo != b.Modulo)
+                throw new Exception("Cannot add IntegerModulos of different modulos.");
+            return new IntegerModulo(a.Modulo, a.Value - b.Value);
+        }
+
+        public IntegerModulo Inverse()
+        {
+            if (Euclid.GreatestCommonDivisor(Value, Modulo) != 1)
+            {
+                return null;
+            }
+            for (var i = new IntegerModulo(Modulo, 1); i.Value <= Modulo; i.Value++)
+            {
+                if ((i*this).Value == 1)
+                    return i;
+            }
+            return null;
+        }
+    }
+}
---
+++ Eric_Crypto_Library/Eric_Crypto_Library_Test/IntegerModuloTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Eric_Crypto_Library;
+using NUnit.Framework;
+
+namespace Eric_Crypto_Library_Test
+{
+    [TestFixture]
+    public class IntegerModuloTest
+    {
+        [Test]
+        public void TestConstruction()
+        {
+            Assert.Throws<ArgumentException>(() => new IntegerModulo(-1, 1));
+            Assert.Throws<ArgumentException>(() => new IntegerModulo(0, 1));
+
+            var test = new IntegerModulo(1, 5);
+            Assert.AreEqual(1, test.Modulo);
+            Assert.AreEqual(0, test.Value);
+
+            test = new IntegerModulo(5, 1);
+            Assert.AreEqual(5, test.Modulo);
+            Assert.AreEqual(1, test.Value);
+
+            test.Value = 5;
+            Assert.AreEqual(0, test.Value);
+
+            test.Value = -6;
+            Assert.AreEqual(4, test.Value);
+        }
+
+        [Test]
+        public void TestAddition()
+        {
+            var a = new IntegerModulo(5, 3);
+            var b = new IntegerModulo(5, 1);
+            var c = new IntegerModulo(4, 1);
+
+            var actual = a + b;
+            Assert.AreEqual(4, actual.Value);
+
+            b.Value = 2;
+            actual = a + b;
+            Assert.AreEqual(0, actual.Value);
+
+            b.Value = 3;
+            actual = a + b;
+            Assert.AreEqual(1, actual.Value);
+
+            Assert.Throws<Exception>(() => actual = a + c);
+        }
+
+        [Test]
+        public void TestMultiplication()
+        {
+            var a = new IntegerModulo(5, 3);
+            var b = new IntegerModulo(5, 3);
+            var c = new IntegerModulo(4, 2);
+
+            var actual = a * b;
+            Assert.AreEqual(4, actual.Value);
+
+            b.Value = 4;
+            actual = a * b;
+            Assert.AreEqual(2, actual.Value);
+
+            Assert.Throws<Exception>(() => actual = a*c);
+        }
+
+        [Test]
+        public void TestSubtraction()
+        {
+            var a = new IntegerModulo(5, 3);
+            var b = new IntegerModulo(5, 3);
+            var c = new IntegerModulo(4, 2);
+
+            var actual = a - b;
+            Assert.AreEqual(0, actual.Value);
+
+            b.Value = 4;
+            actual = a - b;
+            Assert.AreEqual(4, actual.Value);
+
+            Assert.Throws<Exception>(() => actual = a - c);
+        }
+
+        [Test]
+        public void TestInverse()
+        {
+            var a = new IntegerModulo(5, 3);
+            var actual = a.Inverse();
+            Assert.AreEqual(2, actual.Value);
+
+            a.Value = 2;
+            actual = a.Inverse();
+            Assert.AreEqual(3, actual.Value);
+
+            a.Value = 4;
+            actual = a.Inverse();
+            Assert.AreEqual(4, actual.Value);
+
+            a = new IntegerModulo(6, 2);
+            Assert.Null(a.Inverse());
+        }
+    }
+}
