91e6f35f0f7ee923c8851b7decb9fc97d90f7215
Eric Lovell <eric1992@vt.edu>
2014-09-16 16:16:28 -0400

    Started Working on Hill Cipher

---
+++ Eric_Crypto_Library/Eric_Crypto_Library/Keys/HillCipherKey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MathNet.Numerics;
+
+namespace Eric_Crypto_Library.CryptoSystems
+{
+    class HillCipherKey
+    {
+    }
+}
